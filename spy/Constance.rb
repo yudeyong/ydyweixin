55b41d41833a55cce8385d07cf5660f720658364
yudeyong <yudeyong@magus-soft.com>
2014-02-12 23:10:57 +0800

    finish init group function

--- spy/Constance.rb
+++ spy/Constance.rb
@@ -2,4 +2,31 @@
 class Constance
     KEY_QUES = "QUES:"
     KEY_QUES_COUNT = KEY_QUES+"COUNT"
+    KEY_S_GROUPS = "GROUP" #set[groupid1,groupid2]
+    KEY_L_G_Q = "G:Q:" #list[S_HUM,S_IDT,S_GOS], 组,问题
+    KEY_L_G_S = "G:S:" #list, 组,身份, #当局用户数个元素
+    KEY_L_G_U = "G:U:" #组,用户, #当局用户数+1个元素
+    KEY_GRP_COUNT = "G:U:COUNT" #该组当前未获取问题用户数
+    KEY_L_USR = "USR:"
+    #= list[groupid,答案,身份] #身份{人,鬼,白痴,owner}
+    #= list[groupid,当前局人数(不算owner),owner] #if 身份=owner
+    S_HUM=0
+    S_IDT=1
+    S_GOS=2
+    S_OWN=3
+    STATUS_ = ["人", "白痴", "鬼", "own"]
+    
+    def self.shuffle(list)
+        l = list.length
+        cur = 0
+        l.downto(1){|i|
+            j = rand(i);
+            k = i-1
+            list[k],list[j] = list[j],list[k];
+        }
+    end
 end
+
+#l = [1,2,3,4]
+#Constance::shuffle(l)
+#p l
--- spy/spygroup.rb
+++ spy/spygroup.rb
@@ -5,15 +5,17 @@ require "./Constance"
 
 class Spygroup
     # include Singleton
-    
+    DENSITY = 7 #60%密度
+    #
     def self.initialize
         r = Redis.new
+        r.flushdb
         loadSpyDb(r)
-        
+
         r
     end
     @@r = self.initialize
-
+    @@count = 0
     # str : 词语数组,如果数组长度2,str[0]人的词,str[1]白痴的词,鬼的内容只有人的词的字数,如果长3,str[2],鬼的描述
     # ownerid : 创建者id
     # human :人的数,包含1个白痴
@@ -22,12 +24,12 @@ class Spygroup
         self.releaseLast(ownerid)
         self.createGroup(str, ownerid, human, ghost)
     end
-
+#
     private
     def initialize
         p "deprecated method, instance should be creat by getNewGroup"
     end
-
+#
     def self.getWord(str)
         if str==nil
             return @@r[Constance::KEY_QUES+rand(@@r[Constance::KEY_QUES_COUNT].to_i).to_s]
@@ -41,20 +43,64 @@ class Spygroup
                 nil
         end
     end
+#
     def self.createGroup(str, ownerid, human, ghost)
         if (s = self.getWord(str))==nil
             return nil
         end
-        id = getNewGroupID(ownerid)
-p s
+        gid = getNewGroupID(ownerid)
+        initGroup(gid, ownerid,human,ghost,s )
     end
+#
+    def self.initGroup(g, ownerid, human,ghost,s )
+        gid = g.to_s
+        @@r.sadd(Constance::KEY_S_GROUPS, gid)
+
+        total = human+ghost
+        @@r.rpush(Constance::KEY_L_USR+ownerid, [gid,(total), Constance::STATUS_[Constance::S_OWN]])
+
+        w = s.split(',')
+        @@r.rpush(Constance::KEY_L_G_Q+gid,w)
+
+        l = []
+        l[0]=Constance::S_IDT
+        (human-1).downto(1){|x|l[x]=Constance::S_HUM}
+        (total-1).downto(human){|x|l[x]=Constance::S_GOS}
+        Constance::shuffle(l)
+#p l
+        @@r.rpush(Constance::KEY_L_G_S+gid, l )
+
+        @@r.rpush(Constance::KEY_L_G_U+gid, ownerid )
+
+        @@r[Constance::KEY_GRP_COUNT+gid] = total
+    end
+#
     def self.releaseLast(id)
     end
+#
     def self.getNewGroupID(id)
-        
+        i = 10
+        d = DENSITY
+        while @@count > d do
+            i *= 10
+            d *= 10
+        end
+        gid = generateID(id, i)
     end
+#
+    def self.generateID(id, range)
+        i = rand(range)
+        while !@@r.sadd(Constance::KEY_S_GROUPS, i) do
+            i += 1
+        end
+
+        @@count+=1
+        #p @@count.to_s + ","+i.to_s + ","+ range.to_s
+        i
+    end
+
 end
 
-s =["a","b","dd"]
-Spygroup.getNewGroup(s, 1, 1, 1)
+s =["a","b阿","dd"]
+10.downto(0){Spygroup.getNewGroup(s, "own1", 3, 2)}
 
