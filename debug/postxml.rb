ec2bb2f787e8764f35af353cfd69d82f3adc6cbc
yudeyong <yudeyong@gmail.com>
2014-02-13 23:38:52 +0800

    erb post debug data

--- debug/postxml.rb
+++ debug/postxml.rb
@@ -1,21 +1,30 @@
 require "net/http"
 require "uri"
+require "erb"
 
+class Debugwx
 BOUNDARY = "AaB03x"
-def req(fromUser, content)
+
+def req(uid, content)
     uri = URI.parse('http://localhost/hi?timestamp=df&nonce=3&signature=ab25b59a6311e46a4da23644995b858d2f8bdae8&echostr=f')
     file = "wx.xml"
 
+	s = File.read("require.erb")
+	account = "ac"
+	b = binding
+
+    ERB.new(s, 0, "", "s").result b
+ 
+#puts s
     # Token used to terminate the file in the post body. Make sure it is not
     # present in the file you're uploading.
-    time = Time.now
     
     post_body = []
     post_body << "--#{BOUNDARY}\r\n"
     post_body << "Content-Disposition: form-data; name=datafile; filename='#{File.basename(file)}'\r\n"
     post_body << "Content-Type: text/xml\r\n"
     post_body << "\r\n"
-    post_body << File.read(file)
+    post_body << s
     post_body << "\r\n--#{BOUNDARY}--\r\n"
 
 puts post_body
@@ -32,4 +41,7 @@ puts post_body
     p resp['content-type']
     puts resp.body
 end
-req("u1", "new")
+end
+if __FILE__ == $0
+	Debugwx.new.req("u1", "new")
+end
