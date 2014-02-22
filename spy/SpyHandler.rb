0c28789a491fa439e151a204072f664b9bbbe34e
yudeyong <yudeyong@magus-soft.com>
2014-02-22 21:17:03 +0800

    always finish killer game and voting function for both Spy game and killer game. there are bugs, under testing still

--- spy/SpyHandler.rb
+++ spy/SpyHandler.rb
@@ -7,9 +7,13 @@ class SpyHandler
     AUTOREPLY = "指令错误:\n输入：!new 总游戏人数 卧底人数 平民词汇 白痴词汇 卧底词汇\n--新开局， 最后一个卧底词可以不输入，3个词也可都不输入，自动获取游戏词,此时创建者也作为游戏成员计数。\n例：\n!new 6 2 香蕉 菠萝 水果\n\n输入:局代码，获取角色  \n\n输入:!v 人员编号(投票谁为卧底)\n例:\n!v 2 \n\n输入: !next 结束当前轮投票,返回投票结果"
     PARAM_WRONG1 = "需要总人数和卧底数， 例：\n！new 8 2"
     PARAM_WRONG2 = "参数个数不对，如果指定词汇至少需要2个词汇，也可以是3个，例：\n!new 9 3 香蕉 芭蕉 水果"
+    PARAM_WRONG_KILLER = "需要总人数,,例:\n !killer 8 \n或 \n!killer 9 2\n9个人,2警2匪5平民"
+    PARAM_WRONG_KILLERNo = "杀手比例错误."
+    PARAM_WRONG_KTOTALNo = "游戏人数错误.(6-19)"
     TOOMUCHSPY = "卧底太多了吧"
     TOOMUCHPERSON = "人也太多了吧"
     TOOLESSPERSON = "人也太少了吧"
+    
 	#字符串是否为数字
 	def self.isI(str)
 		(str=~/^\d+?$/)!=nil
@@ -21,9 +25,13 @@ class SpyHandler
     
     #指令，函数映射表
     COMMAND = {
-		'new'=>'newGame',
-        'v'=>'vote',
-        'next'=>'finishVote'
+		'new'=>'newGame', #新建卧底游戏
+        'v'=>'vote',    #投票
+        'nextf'=>'finishVoteForce',   #强制完成投票,进入下一轮
+        'next'=>'finishVote',   #完成投票,进入下一轮
+        'kill'=>'killbyno', #杀一人
+        'killer'=>'killGame',   #新建杀手游戏
+        'revote'=>'revote', #重新投票
 		}
 	
     private
@@ -36,6 +44,17 @@ class SpyHandler
         return addUser(cmd,xml.elements["FromUserName"].text)
 	end
 	
+    #
+    def createGame(uid, data)
+        return PARAM_WRONG1 if  !self.class.isI(data[0]) || !self.class.isI(data[1])
+        total = data.shift.to_i
+        spy = data.shift.to_i
+        return TOOLESSPERSON if total<4
+        return TOOMUCHPERSON if total>20
+        return TOOMUCHSPY if total<(spy<<1)
+        Spygroup::getNewGroup(uid, total-spy,spy,data )
+        
+    end
 	#格式化content返回值=>xml
     def formatResponse( xml, content)
 		xml.elements["FromUserName"].text , xml.elements["ToUserName"].text = xml.elements["ToUserName"].text , xml.elements["FromUserName"].text  
@@ -50,12 +69,18 @@ class SpyHandler
     #指令处理
     def parseCMD(cmd, uid)
 		i = cmd.index(' ')
+        i = cmd.length if i==nil
 		str = ((i) && i>0)?cmd[0..(i-1)] : (i=0,cmd)
-		return AUTOREPLY if (COMMAND[str]==nil) 
+        str.downcase!()
+		return AUTOREPLY if (COMMAND[str]==nil)
 		send(COMMAND[str],uid,cmd[i,cmd.length])
     end
     #完成本轮投票
     def finishVote(uid, deprecated)
+        Spygroup::finishVote(uid,false)
+    end
+    def finishVoteForce(uid, deprecated)
+        Spygroup::finishVote(uid,true)
     end
     # result, 怀疑对象userid
     def vote(uid,result="")
@@ -71,15 +96,28 @@ class SpyHandler
 		else PARAM_WRONG2
 		end
     end
-    #
-    def createGame(uid, data)
-		return PARAM_WRONG1 if  !self.class.isI(data[0]) || !self.class.isI(data[1])
-		total = data.shift.to_i
-		spy = data.shift.to_i
-		return TOOLESSPERSON if total<4 
-		return TOOMUCHPERSON if total>20
-		return TOOMUCHSPY if total<(spy<<1) 
-		Spygroup::getNewGroup(uid, total-spy,spy,data )
-		
+#
+    def killbyno(uid, killedno)
+        Spygroup::killbyno(uid,killedno)
+    end
+#
+    def killGame(uid, cmd)
+		data = cmd.split(' ')
+        return PARAM_WRONG_KILLER if data.length<=0 || data.length>2
+        data.each{|x| return PARAM_WRONG_KILLER unless self.class.isI(x)}
+        total = data[0].to_i
+        if data.length>1
+            spy = data[1].to_i
+            return PARAM_WRONG_KILLERNo if (spy *3+1) >=total
+        else
+            case total
+            when 6..8 then spy = 1
+            when 9..11 then spy = 2
+            when 12..15 then spy = 3
+            when 16..19 then spy = 4
+            else return PARAM_WRONG_KTOTALNo
+            end
+        end
+        Spygroup::getNewGroup(uid, total-spy,spy,nil, true )
     end
 end
--- spy/spygroup.rb
+++ spy/spygroup.rb
@@ -27,10 +27,10 @@ class Spygroup
     # ownerid : 创建者id，此人不任游戏角色，裁判
     # human :人的数,包含n个白痴，n=详见initGroup
     # spy :鬼的数
-    def self.getNewGroup( ownerid, human, spy, str=[])
+    def self.getNewGroup( ownerid, human, spy, str=[], isKiller=false)
         self.releaseGroupbyUsr(ownerid)
         self.releaseExpiration(Constance::CHECK_EXPIRATION_COUNT)#每新局,检查部分过期数据并释放
-        self.createGroup(str, ownerid, human, spy)
+        self.createGroup(str, ownerid, human, spy, isKiller)
     end
 	#release expired group
 	#count: check count, 0 for all
@@ -81,7 +81,7 @@ t = Time.now.to_i() *0#for debug
 
     def self.getResult(gid,result)
         a = Array.new(result.length+1,0)
-        max = 0
+        max = 1
         #统计得票,并得到最大值
         result.each do |x|
             if (x=x.to_i)>0
@@ -91,16 +91,17 @@ t = Time.now.to_i() *0#for debug
         end
         
         round=-1
-#        count=0
+        count=0
         (result.length-1).downto(0) do |x|
             if a[x+1]<max
                 result[x]= VOTE_INIT_S if result[x].to_i>0#(result[x].to_i>0 ? -1 : (round=result[x].to_i - 1))
                 else
                 result[x]=max
-                #count += 1
+                count += 1
             end
             @@r.lset(Constance::KEY_L_G_VT+gid,x,result[x])
         end
+        count
     end
 VOTE_INIT_S = 0 #投票者初始状态
 VOTE_BLOCK = -1 #暂时不能被投票的人,比如某轮有>1个人获得票数相同,需要进一步讨论,其他低票数用户在再投票环节暂时不能被投
@@ -109,42 +110,103 @@ VOTE_DEAD = -2 #死者,不能投也不能被投
     # v:被投票人编号
     def self.vote(uid, v)
         usr = @@r.lrange(Constance::KEY_L_USR+uid,0,-1)
-        return "不存在你的数据" if ! (gid = usr && usr[0])
-        a = @@r.get(Constance::KEY_VOTE+gid)
+        return :"不存在你的数据" if ! (gid = usr && usr[0])
+p         a = @@r.get(Constance::KEY_VOTE+gid)
+        result = @@r.lrange(Constance::KEY_L_G_VT+gid,0,-1)#注意用户编号从1开始
         if (a.to_i()>0)
-            result = @@r.lrange(Constance::KEY_L_G_VT+gid,0,-1)#注意用户编号从1开始
             return :"参数不正确,请输入1-#{result.length}的数字" if SpyHandler::isI(v) || result.length<(v =v.to_i) || v<=0
             #usr[3].to_i() - 1 ==>No. 投票人编号
-            self.votework(gid,v,result,usr[3].to_i() - 1).to_s + self.getVoteResult(gid)
-            else
-                self.getVoteResult(gid,result)
+            return "此人不能投票" if usr.length<=3
+            self.votework(gid,v,result,usr[3].to_i() - 1).to_s + self.getVoteResult(gid,result)
+        else
+            self.getVoteResult(gid,result)
         end
+    end
+
+    def self.killbyno(uid, v )#v - killedno
+        usr = @@r.lrange(Constance::KEY_L_USR+uid,0,-1)
+        return :"不存在你的数据" if ! (gid = usr && usr[0])
+        result = @@r.lrange(Constance::KEY_L_G_VT+gid,0,-1)#注意用户编号从1开始
+        return :"参数不正确,请输入1-#{result.length}的数字" if SpyHandler::isI(v) || result.length<(v =v.to_i) || v<=0
+#当KEY_VOTE>0游戏中,任意人非死人可被杀
+#当KEY_VOTE==0投票结束,此时KEY_L_G_VT[n]=0代表此人投票不是最高票,不能被杀
+        v-=1#用户编号从1开始,VT从0开始
+        return "此人不能被杀,手下留情." if result[v].to_i<=(VOTE_INIT_S+(((i=@@r.get(Constance::KEY_VOTE+gid).to_i)>0) ? -1 : 0 ))
+#kill killedno guy
+        @@r.lset(Constance::KEY_L_G_VT+gid,v,VOTE_DEAD)
+        result[v] = VOTE_DEAD
+#p result,v,i
+        i>0 ? @@r.decr(Constance::KEY_VOTE+gid) : self.nextTurn(gid,result)
+        "#{v+1}号被杀,请继续"
+    end
+#
+    def self.finishVote(uid,isforce)
+        usr = @@r.lrange(Constance::KEY_L_USR+uid,0,-1)
+        return :"不存在你的数据" if ! (gid = usr && usr[0])
+        return "还有人未投票,强行进入下一轮,请用nextf指令" if !isforce && (@@r.get(Constance::KEY_VOTE+gid).to_i)>0
+        result = @@r.lrange(Constance::KEY_L_G_VT+gid,0,-1)#注意用户编号从1开始
+        if isforce
+            count = getResult(gid, result)
+        else
+            count = 0
+            result.each{|x| count+=1 if x.to_i>0 }
+        end
+        s = self.getVoteResult(gid,result,isforce)
+puts "before"+result.to_s
+
+        if flag = (!isforce && count==1)
+            i=result.length-1
+            while i>=0
+                if result[i].to_i>0
+                    @@r.lset(Constance::KEY_L_G_VT+gid,i,VOTE_DEAD)
+                    result[i] = VOTE_DEAD
+                    break;
+                end
+                i-=1
+            end
+        end
+        self.nextTurn(gid,result)
+p result
+        s += flag ? "\n得票最高者已经被清除,游戏继续." : "\n请通过kill no.手动清除需要清除的玩家"
     end
 ##########################
 private
+    def self.nextTurn(gid,result)
+        count = 0
+        (result.length-1).downto(0)do |x|
+            if result[x].to_i>=0
+                count += 1 ;
+                @@r.lset(Constance::KEY_L_G_VT+gid,x,VOTE_INIT_S)
+                else
+                p x
+            end
+        end
+        @@r.set(Constance::KEY_VOTE+gid, count)
+    end
     def self.votework(gid, v,result,u)
-p result
         return "您已经投过票了." if result[u].to_i>VOTE_INIT_S
         return "死人消停一下." if result[u].to_i==VOTE_DEAD
-        return "此人不能被投,手下留情." if result[v].to_i<VOTE_INIT_S
-#p Constance::KEY_L_G_VT+gid, u, i
+        return "此人不能被投,手下留情." if result[v-1].to_i<VOTE_INIT_S
+#p Constance::KEY_L_G_VT+gid, u,v
         result[u] = v
         @@r.lset(Constance::KEY_L_G_VT+gid, u, v )
         if @@r.decr(Constance::KEY_VOTE+gid)==0 #投票结束,计算结果
             self.getResult(gid,result)
         end
-#@@r.set
+#@@r.lindex(Constance::KEY_L_G_VT+gid,u)
     end
     #optimized prompt
     def self.getText(c,no,flag)
 		"\n你的编号为: #{no}"+(c>0 ? "\n还有#{c}人未加入" : (flag ? "\n人齐，游戏开始" : ""))
     end
-    def self.getVoteResult(gid,result)
-       if ( a = @@r.get(Constance::KEY_VOTE+gid).to_i>0)
+    def self.getVoteResult(gid,result,isforce=false)
+       if ( (a = @@r.get(Constance::KEY_VOTE+gid).to_i)>0) && !isforce
            "\n还有#{a}人未投票"
            else
-           #(result.length-1)downto(0){|x|}
-           "\n完成"
+            s = "\n投票结果:"
+            #p result
+           (result.length-1).downto(0){|x| s+="\n编号#{x+1}获最高票" if result[x].to_i>VOTE_INIT_S}
+           s
         end
     end
     
@@ -173,41 +235,46 @@ p result
 	#--词组内容
 	#--用户身份
 	#--返回组id
-    def self.createGroup(str, ownerid, human, spy)
-
+    def self.createGroup(str, ownerid, human, spy,isKiller)
+        str = ["平民", "警", "匪"]   if isKiller #killer game
         if (s = self.getWord(str))==nil
             return nil
         end
         gid = getNewGroupID(ownerid)
-        initGroup(gid, ownerid,human,spy,s , str.length==0).to_s+"游戏编号:#{gid}\n其他人可输入此编号，开始游戏。"
+        initGroup(gid, ownerid,human,spy,s , str.length==0, isKiller).to_s+"游戏编号:#{gid}\n其他人可输入此编号，开始游戏。"
     end
 	#创建redis中组相应的数据
     #isAutoWords
     #   true: 自动获取,开局者也参与游戏,总人数包括开局者,否则不包括
-    def self.initGroup(g, ownerid, human,spy,s, isAutoWords)
+    def self.initGroup(g, ownerid, human,spy,s, isAutoWords, isKiller)
         gid = g.to_s
 
         total = human+spy
         #owner[ gid, total, "owner"]
         w = s.split(',')
-        w[Constance::S_SPY] = "你是卧底,提示:"+w[Constance::S_SPY]
-        @@r.rpush(Constance::KEY_L_G_Q+gid,w)
 
         l = []
-        if isAutoWords then
-
-            i = spy
-            human = total
-        else
+        if isKiller
             @@r.rpush(Constance::KEY_L_USR+ownerid, [gid,"总人数：#{total}", Constance::STATUS_[Constance::S_OWN]])
-        
-            #白痴生成算法
-            #3人：2人，0白，1卧
-            #4-10人：1白
-            #11-20人：2白
-            #>20人：人太多了
-            i = (total+3)/7
+            i = spy
+        else
+            w[Constance::S_SPY] = "你是卧底,提示:"+w[Constance::S_SPY]
+            if isAutoWords then
+                i = spy
+                human = total
+            else
+                @@r.rpush(Constance::KEY_L_USR+ownerid, [gid,"总人数：#{total}", Constance::STATUS_[Constance::S_OWN]])
+            
+                #白痴生成算法
+                #3人：2人，0白，1卧
+                #4-10人：1白
+                #11-20人：2白
+                #>20人：人太多了
+                i = (total+3)/7
+            end
         end
+        @@r.rpush(Constance::KEY_L_G_Q+gid,w)
+
         (i-1).downto(0){|x|l[x]=Constance::S_IDT}
         (human-1).downto(i){|x|l[x]=Constance::S_HUM}
         (total-1).downto(human){|x|l[x]=Constance::S_SPY}
@@ -236,15 +303,15 @@ if DBG_RELEASE
     p @@r.keys("U*"),@@r.zrange("GROUP",0,-1)
     p @@r.keys("G*"),@@count.to_s+";"+gid.to_s
 end
-    usrs = @@r.lrange(Constance::KEY_L_G_U+gid,0,-1)
-    @@r.del(Constance::KEY_L_G_Q+gid)
-    @@r.del(Constance::KEY_L_G_S+gid)
-    @@r.del(Constance::KEY_GRP_COUNT+gid)
-    @@r.del(Constance::KEY_L_G_VT+gid)
-    @@r.del(Constance::KEY_VOTE+gid)
-    usrs.each{|x|@@r.del(Constance::KEY_L_USR+x)}
-    @@r.del(Constance::KEY_L_G_U+gid)
-    @@count -= 1 if (@@r.zrem(Constance::KEY_Z_GROUPS, gid))
+        usrs = @@r.lrange(Constance::KEY_L_G_U+gid,0,-1)
+        @@r.del(Constance::KEY_L_G_Q+gid)
+        @@r.del(Constance::KEY_L_G_S+gid)
+        @@r.del(Constance::KEY_GRP_COUNT+gid)
+        @@r.del(Constance::KEY_L_G_VT+gid)
+        @@r.del(Constance::KEY_VOTE+gid)
+        usrs.each{|x|@@r.del(Constance::KEY_L_USR+x)}
+        @@r.del(Constance::KEY_L_G_U+gid)
+        @@count -= 1 if (@@r.zrem(Constance::KEY_Z_GROUPS, gid))
 if DBG_RELEASE
     p @@r.keys("U*"),@@r.zrange("GROUP",0,-1)
     p @@r.keys("G*"),@@count.to_s+";"+gid.to_s
